df66decfecde1eebdf5b5bbe59421b42fcc4fbcb
Slavi <slavi_14@abv.bg>
2019-02-13 16:09:06 +0200

    jdbc-training

---
+++ JDBC/src/main/java/com/example/mydemo/models/Employee.java
@@ -0,0 +1,48 @@
+package com.example.mydemo.models;
+
+public class Employee {
+    private int id;
+    private String firstName, lastName, jobTitle;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String firstName, String lastName, String jobTitle) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.jobTitle = jobTitle;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+}
