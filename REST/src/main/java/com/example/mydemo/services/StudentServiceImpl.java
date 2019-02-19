d20386509b8b86786a68a12ae3b71de3abbe6804
Slavi <slavi_14@abv.bg>
2019-02-19 20:53:52 +0200

    rest-training

--- REST/src/main/java/com/example/mydemo/services/StudentServiceImpl.java
+++ REST/src/main/java/com/example/mydemo/services/StudentServiceImpl.java
@@ -23,7 +23,7 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public List<Student> getStudents() {
-        return null;
+        return repository.getStudents();
     }
 
     @Override
